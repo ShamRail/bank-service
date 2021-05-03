8f2103e513e3ac778573af826f54ddc41a25f2c2
ShamRail <rail1999g102r@mail.ru>
2021-05-03 10:50:29 +0500

    Updated poms

--- src/test/java/ru/job4j/bank/service/service/BankServiceImplTest.java
+++ src/test/java/ru/job4j/bank/service/service/BankServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.job4j.bank.service.BankServiceApplication;
 import ru.job4j.bank.service.model.Credit;
 import ru.job4j.bank.service.model.Passport;
 
